4a887558f504b9444f7a4da25d93a018107a60b1
emersonjorquera <141077939+emersonjorquera@users.noreply.github.com>
2023-08-14 18:31:06 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,10 @@
+function alerta_boton(){
+    alert("Hola");
+}
+function alerta_div(evento){
+    alert("Hola! Soy el div");
+}
+
+let div = document.getElementById("div");
+div.addEventListener("click", alert("Hola! Soy el div"));
+  p.onclick = alerta_div; 
